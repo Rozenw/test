c0a5e4766ccd7b517a67132fcff9535cb46c547f
Rozenw <Rozenw@Bucket>
2019-01-28 13:37:34 +0300

    Some stuff from chdir command on java.

--- src/chdir_fd/Chdir_fd.java
+++ src/chdir_fd/Chdir_fd.java
@@ -9,19 +9,36 @@ public class Chdir_fd {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         int cdup_cnt = 0;
+        boolean except = false;
         
         if (line.startsWith("cd ")) { 
             line = line.substring(3);
             String[] pathAr = line.split("/");
-            for (int k = 0; k < pathAr.length; k++) {
-                if (pathAr[k].equals("..")) {
-                cdup_cnt++;
+            
+            int exCount = 0;
+            while (!pathAr[exCount].equals("..")) {
+                exCount++;
+            }
+            for (int p = exCount; p < pathAr.length; p++) {
+                if (!pathAr[p].equals("..")) {
+                except = true;
                 }
             }
-            for (int i = 0; i < (pathAr.length - (cdup_cnt * 2)); i++) {        
-                System.out.print(pathAr[i] + "/");
+            
+            if (!except) {
+                for (int k = 0; k < pathAr.length; k++) {
+                    if (pathAr[k].equals("..")) {
+                    cdup_cnt++;
+                    }
+                }
+                for (int i = 0; i < (pathAr.length - (cdup_cnt * 2)); i++) {        
+                    System.out.print(pathAr[i] + "/");
+                }
+                System.out.println("");
             }
-            System.out.println("");
+            else {
+                System.out.println("Sorry. I don't understand you.");
+            }
         }
         else {
             System.out.println("I don't understand you. Please enter the command..");
